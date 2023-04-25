508337f1c938fd430b5f5375925f8dbccc6e9508
MaestroAshmin <ashmin.karki1996@gmail.com>
2023-04-25 14:06:24 +1000

    Removed ImageViewer Component

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-import { ImageViewer } from "react-image-viewer-dv";
 import logo from "./logo.png";
 export default class Nav extends Component {
   render() {
     return (
       <div class="banner">
-        <ImageViewer>
-          <img src={logo} alt="Swinburne Online logo"></img>
-        </ImageViewer>
+        <img src={logo} alt="Swinburne Online logo"></img>
         <h1>Swinburne Online FAQ Bot</h1>
       </div>
     );
